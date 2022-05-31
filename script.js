f34ebb2b0194896286de1cd0a327ffaf3eca4bcd
philipn778 <philipncontact@gmail.com>
2022-05-31 07:00:24 +0000

    modified:   script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
 const SELECTIONS = [
 {
 name: 'rock',
@@ -27,5 +30,33 @@ selectionButtons.forEach(selectionButton => {
 })
 
 function makeSelection(selection) {
-    console.log(selection)
+    const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner (computerSelection, selection)
+    
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
+    if (yourWinner) incrementScore(yourScoreSpan)
+    if (computerWinner) incrementScore(computerScoreSpan)
+}
+
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+finalColumn.after(div)
+}
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
 }
